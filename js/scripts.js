887bcf3b73c604aa2f09a5430cc89d4d390b5e02
Jonathan Buchner and vichitra pool <vp:notejde@gmail.com jb:me@jonathanbuchner.com>
2016-08-10 11:16:31 -0700

    example of forms

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+  $("#blanks form").submit(function(event) {
+    var person1 = $("#person1").val();
+    $(".person1").text(person1);
+    var person2 = $("#person2").val();
+    $(".person2").text(person2);
+    var a = $("#animal").val();
+    $(".animal").text(a);
+    var e = $("#exclamation").val();
+    $(".exclamation").text(e);
+    var v = $("#verb").val();
+    $(".verb").text(v);
+    var n= $("#noun").val();
+    $(".noun").text(n);
+
+
+    $("#story").show();
+    event.preventDefault();
+  });
+});
